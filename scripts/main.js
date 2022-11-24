e1e89e2ba7bf639df0ce77d375794eb2d030fdc1
Cristian Ricardo Parra <116090196+cristianrparra@users.noreply.github.com>
2022-11-24 14:32:46 +0100

    updated file format

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const contactButton = document.getElementById('contact--form-button');
+	
+const exitButton = document.getElementById('contact--form-exit');
+	
+const contactForm = document.getElementById('contact-form');
+	
+contactButton.addEventListener('click', () => {
+
+	contactForm.style.display = 'grid';
+
+});
+	
+exitButton.addEventListener('click', () => {
+
+	contactForm.style.display = 'none';
+
+});
